5fc4a2023726de84d7d7aeb645f4646243fed4f6
Elizabeth <91484596+elzbthg@users.noreply.github.com>
2023-07-16 19:55:10 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,3 +1,14 @@
+//ITCS227 Source Code Template for AT AY 2022-2023
+/*
+
+		Program: E-commerce API
+		Programmer: Maria Elizabeth Genabe
+		Section: AN22
+		Start Date: July 16, 2023
+		End Date: July 17, 2023
+
+*/
+
 const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
